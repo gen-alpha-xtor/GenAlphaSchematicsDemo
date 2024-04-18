1dfbad57739b25ee009e89f66cc8e8044178b11c
Dan Fritchman <dan@fritch.mn>
2024-04-18 03:32:44 +0000

    Lib defs becomes an include rather than a symlink. Add script-format env vars.

---
+++ env.sh
@@ -0,0 +1,12 @@
+# 
+# Schematic Environment
+# In script form
+# 
+
+PDK_DIR=${PWD}/asap7_pdk_r1p7
+
+# GenAlpha doesn't care about this one. 
+# But some other editors might! 
+CDS=/tools/cadence/ICADVM/ICADVM201
+
+
